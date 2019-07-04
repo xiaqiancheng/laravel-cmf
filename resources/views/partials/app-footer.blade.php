c840252f392c41b13dce7755fbeca2c3ca852a3c
夏前程(孤狼) <v.xiaqc@yoozoo.com>
2019-07-04 15:53:20 +0800

    修改footer

--- resources/views/partials/app-footer.blade.php
+++ resources/views/partials/app-footer.blade.php
@@ -3,7 +3,7 @@
         @if (rand(1,100) == 100)
             <i class="laravel-cmf-rum-1"></i> {{ __('laravel-cmf::theme.footer_copyright2') }}
         @else
-            {!! __('laravel-cmf::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
+            {!! __('laravel-cmf::theme.footer_copyright') !!} <a href="https://github.com/xiaqiancheng/laravel-cmf" target="_blank">Laravel Cmf</a>
         @endif
         @php $version = LaravelCmf::getVersion(); @endphp
         @if (!empty($version))
